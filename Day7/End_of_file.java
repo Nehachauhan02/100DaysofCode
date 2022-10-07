b9af50b80a2f347652c7f97ee9002bde7d58a71b
Nehachauhan02 <114774299+Nehachauhan02@users.noreply.github.com>
2022-10-07 22:21:43 +0530

    Update End_of_file.java

--- Day7/End_of_file.java
+++ Day7/End_of_file.java
@@ -1,8 +1,10 @@
-//The challenge here is to read n lines of input until you reach EOF, then number and print all n lines of content.
+/*The challenge here is to read n lines of input until you reach EOF, then number and print all n lines of content.
+Input Format
+Read some unknown n lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
 
-
-
-
+Output Format
+For each line, print the line number, followed by a single space, and then the line content received as input.
+*/
 
 import java.io.*;
 import java.util.*;
