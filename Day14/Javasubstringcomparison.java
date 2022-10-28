d9c9f7878031e0e65adeb97b8cd599a94cfae075
Nehachauhan02 <114774299+Nehachauhan02@users.noreply.github.com>
2022-10-28 23:27:59 +0530

    Update Javasubstringcomparison.java

--- Day14/Javasubstringcomparison.java
+++ Day14/Javasubstringcomparison.java
@@ -1,3 +1,5 @@
+//Given a string,s, and an integer,k, complete the function so that it finds the lexicographically smallest and largest substrings of length k.
+
 import java.util.Scanner;
 
 public class Solution {
